6982ebed98ed52718ffe96c0ede508d637a8b871
a362389462 <1728771259@qq.com>
2022-06-30 05:06:07 +1000

    push

--- flask_app.py
+++ flask_app.py
@@ -20,24 +20,25 @@ def hello_world():
 
 @hops.component(
     "/Duplicator",
-    name="Duplicator"
-    description="Put your building into it an duplicate it"
+    name="Duplicator",
+    description="Put your building into it an duplicate it",
     inputs=[
         hs.HopsBrep("FP","FP","Floor Plan"),
-        hs.HopsNumber("FN","FN","Floor Number")
-        hs.HopsNumber("FH","FH","Floor Hight",Default=1.0)
-        ],
+        hs.HopsNumber("FN","FN","Floor Number"),
+        hs.HopsNumber("FH","FH","Floor Hight",Default=1.0),
+        ]
     outputs=[
         hs.HopsBrep("Result","R","Result")
         ]
 )
 
 def Duplicator(brep,R):
-    FP = hs.HopsBrep("FP","FP","Floor Plan")
-    FN = hs.HopsNumber("FN","FN","Floor Number") 
-    FH = hs.HopsNumber("FH","FH","Floor Hight",Default=1.0)
-    R = hs.HopsBrep("Result","R","Result")
+    FP = hs.HopsBrep("FP","FP","Floor Plan"),
+    FN = hs.HopsNumber("FN","FN","Floor Number") ,
+    FH = hs.HopsNumber("FH","FH","Floor Hight",Default=1.0),
+    R = hs.HopsBrep("Result","R","Result"),
     T = FN * FH
+
     return brep.Duplicator(R)
 
 if __name__ == "__main__":
